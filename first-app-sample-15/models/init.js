6fda8195b26e4785bdea865224e0fd1b5ce83bea
lichen <119449234@qq.com>
2018-12-27 21:26:03 +0800

    install mongodb with docker

--- first-app-sample-15/models/init.js
+++ first-app-sample-15/models/init.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 
-var db = mongoose.connect('mongodb://127.0.0.1:27017/firstapp', {
-  useMongoClient: true
+// var db = mongoose.connect('mongodb://127.0.0.1:27017/firstapp', {
+var db = mongoose.connect('mongodb://127.0.0.1:27017/webapp2', {
+    useMongoClient: true
 });
 
 db.on('error', function(err) {
