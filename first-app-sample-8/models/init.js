6b953b8c3de3052af09cf87b32f891a1b50fe2b7
lichen <119449234@qq.com>
2018-12-31 16:24:15 +0800

    use terminal and kitematic docker connect mongodb

--- first-app-sample-8/models/init.js
+++ first-app-sample-8/models/init.js
@@ -1,7 +1,9 @@
 var mongoose = require('mongoose');
 
-var db = mongoose.connect('mongodb://127.0.0.1:27017/firstapp', {
-  useMongoClient: true
+// var db = mongoose.connect('mongodb://127.0.0.1:27017/firstapp', {
+// 上面的是使用终端docker连接的monggodb，下面的是使用docker的kitematic链接mongodb
+var db = mongoose.connect('mongodb://192.168.99.100:32771/firstapp', {
+    useMongoClient: true
 });
 
 db.on('error', function(err) {
